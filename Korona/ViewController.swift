3e9d254e8512d0329b676df53fb51c9874265453
Ugur <ugur.genc33@gmail.com>
2020-04-07 12:29:51 +0300

    Initial Commit

---
+++ Korona/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Korona
+//
+//  Created by Ugur Genc on 7.04.2020.
+//  Copyright © 2020 Bug Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
